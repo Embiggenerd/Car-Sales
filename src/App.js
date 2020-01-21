08bb325de3bf405a89c6166e4ab3e4cdeb9e9fd9
igor <embiggenerd@gmail.com>
2020-01-21 00:06:07 -0500

    removeFeature invoking on click

--- src/App.js
+++ src/App.js
@@ -12,26 +12,11 @@ const App = () => {
   const additionalFeatures = useSelector(state => state.additionalFeatures)
   const additionalPrice = useSelector(state => state.additionalPrice)
   const dispatch = useDispatch()
-  // const state = {
-  //   additionalPrice: 0,
-  //   car: {
-  //     price: 26395,
-  //     name: '2019 Ford Mustang',
-  //     image:
-  //       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-  //     features: []
-  //   },
-  //   additionalFeatures: [
-  //     { id: 1, name: 'V-6 engine', price: 1500 },
-  //     { id: 2, name: 'Racing detail package', price: 1500 },
-  //     { id: 3, name: 'Premium sound system', price: 500 },
-  //     { id: 4, name: 'Rear spoiler', price: 250 }
-  //   ]
-  // };
 
   const removeFeature = item => {
-
+    console.log('removeFeature invoked', item)
   }
+
   const buyItem = item => {
     dispatch({ type: BUY_ITEM, payload: { item } })
   };
@@ -40,7 +25,7 @@ const App = () => {
     <div className="boxes">
       <div className="box">
         <Header car={car} />
-        <AddedFeatures car={car} />
+        <AddedFeatures handleRemoveFeature={removeFeature} car={car} />
       </div>
       <div className="box">
         <AdditionalFeatures handleAddFeature={buyItem} additionalFeatures={additionalFeatures} />
