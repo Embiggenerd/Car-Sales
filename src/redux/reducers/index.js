b15bf08dc6234141eab7246d4d02e855cab2c5a4
igor <embiggenerd@gmail.com>
2020-01-20 23:55:12 -0500

    feature not able to be double bought

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -22,13 +22,15 @@ export const carReducer = (state = initialState, { type, payload }) => {
         case BUY_ITEM:
             const newAdditionalPrice = state.additionalPrice + payload.item.price 
             const newFeatures = [...state.car.features, payload.item]
+        const newAdditionalFeatures = state.additionalFeatures.filter(f => f.id !== payload.item.id)
             return {
                 ...state,
                 car: {
                     ...state.car,
                     features: newFeatures
                 },
-                additionalPrice: newAdditionalPrice
+                additionalPrice: newAdditionalPrice,
+                additionalFeatures: newAdditionalFeatures
 
             }
         default:
